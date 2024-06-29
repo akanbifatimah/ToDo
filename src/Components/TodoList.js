882b9784da48d102ab665b9a84cb040b35baa47d
Fatimah Akanbi <akanbifatimah@gmail.com>
2024-06-29 21:58:29 +0100

    added css for responsivenes across all screens

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -4,11 +4,13 @@ import './../styles/TodoList.css';
 
 const TodoList = ({ todos, toggleTodo, removeTodo }) => {
   return (
+    <div className='todo-container'>
     <ul className="todo-list">
       {todos.map(todo => (
         <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} removeTodo={removeTodo} />
       ))}
     </ul>
+    </div>
   );
 };
 
