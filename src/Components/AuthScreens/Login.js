6be64f4e3494cbc14d17699b4cb0f8ce8652b6e3
Fatimah Akanbi <akanbifatimah@gmail.com>
2024-06-29 19:22:04 +0100

    Added Login,logout and and authentication screens, implemented logic to endure user is autheticated before accessing the app

---
+++ src/Components/AuthScreens/Login.js
@@ -0,0 +1,53 @@
+
+import React, { useState } from 'react';
+import { useAuth } from '../Context/AuthContext';
+import './../../styles/login.css'; 
+import { useNavigate } from 'react-router-dom';
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const dummyToken = '123456789090'; 
+    login(dummyToken);
+    navigate('/');
+  };
+  
+
+  return (
+    <div className="login-form">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+           
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/Components/AuthScreens/ProtectedRoute.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+import { useAuth } from '../Context/AuthContext';
+
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { isLoggedIn } = useAuth();
+  if (!isLoggedIn) {
+    return (
+        <Navigate
+            to="/login"
+            replace
+        />
+    );
+}
+return <Component {...rest} />;
+};
+
+export default ProtectedRoute;
